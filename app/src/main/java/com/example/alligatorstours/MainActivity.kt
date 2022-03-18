2a746a7c778d55747bd9a72441423c82cf5c81fa
Ricardo Sanchez <rickyelduende@gmail.com>
2022-03-18 16:41:02 -0400

    Change misc.xml

--- app/src/main/java/com/example/alligatorstours/MainActivity.kt
+++ app/src/main/java/com/example/alligatorstours/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.alligatorstours
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_chat.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        floatingActionButton.setOnClickListener{
+            Intent(this,ChatActivity::class.java).also{
+                startActivity(it)
+            }
+        }
     }
 }
