3c00880b94d8a3fb22580d2ac00e583a7296e5f7
Ricardo Sanchez <rickyelduende@gmail.com>
2022-04-04 16:42:00 -0400

    Added google maps plugin to gradle and made changes to manifest (activity and meta data tags)

--- app/src/main/java/com/example/alligatorstours/MainActivity.kt
+++ app/src/main/java/com/example/alligatorstours/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.alligatorstours
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_chat.*
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,6 +11,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toCommonTours = findViewById<Button>(R.id.btnToCommonTours)
+        toCommonTours.setOnClickListener {
+            Intent(this, CommonToursActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+        val toCustomize = findViewById<Button>(R.id.btnCustomize)
+        toCustomize.setOnClickListener {
+            Intent(this, CustomizeActivity::class.java).also {
+                startActivity(it)
+            }
+        }
 
         floatingActionButton.setOnClickListener{
             Intent(this,ChatActivity::class.java).also{
